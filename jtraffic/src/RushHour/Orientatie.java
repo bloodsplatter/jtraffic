d72c51987f34f825d7c75c631f445f0955414015
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-02-13 18:07:31 +0000

    Code version of the first UML model

---
+++ jtraffic/src/RushHour/Orientatie.java
@@ -0,0 +1,36 @@
+package RushHour;
+
+
+/**
+ *  <p style="margin-top: 0">
+ *        De ori&#235;ntatie van het voertuig
+ *      </p>
+ */
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.7BD18BE3-D016-7E1D-4A25-9ABDF1547C5D]
+// </editor-fold> 
+public enum Orientatie {
+
+    /**
+     *  <p style="margin-top: 0">
+     *        Het voertuig staat horizontaal op het speelveld
+     *      </p>
+     */
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.3ACDF043-1C84-2754-70B9-C6D1188844B5]
+    // </editor-fold> 
+    Horizontaal,
+
+    /**
+     *  <p style="margin-top: 0">
+     *        Het voertuig staat verticaal op het speelveld
+     *      </p>
+     */
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.F5300E21-44C6-DDDD-DA70-9DC573C4819B]
+    // </editor-fold> 
+    Verticaal;
+
+
+}
+
