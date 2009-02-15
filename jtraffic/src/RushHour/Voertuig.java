987193ac2dfb47ee59e55e33f4686330b37bec6a
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-02-15 11:57:40 +0000

    Added a lot more basic code, some comments and most of the JavaDoc

--- jtraffic/src/RushHour/Voertuig.java
+++ jtraffic/src/RushHour/Voertuig.java
@@ -34,13 +34,21 @@ public class Voertuig {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.B2EA278E-3163-DF9B-812E-995D2AAD641C]
     // </editor-fold> 
-    public Voertuig () {
+    /**
+     * Constructor
+     */
+    public Voertuig() {
         y = 0;
         x = 0;
         this.grootte = Grootte.Auto;
         this.orientatie = Orientatie.Horizontaal;
     }
 
+    /**
+     * Geparameteriseerde constructor
+     * @param grootte de grootte van het voertuig
+     * @param orientatie de orientatie van het voertuig
+     */
     public Voertuig(Grootte grootte, Orientatie orientatie)
     {
         this.grootte = grootte;
@@ -49,59 +57,106 @@ public class Voertuig {
         y = 0;
     }
 
+    /**
+     * Geparameteriseerde constructor
+     * @param X de horizontale positie van het voertuig in het speelveld
+     * @param Y de verticale positie van het voertuig in het speelveld
+     */
+    public Voertuig(int X, int Y)
+    {
+        this.grootte = Grootte.Auto;
+        this.orientatie = Orientatie.Horizontaal;
+        this.x = X;
+        this.y = Y;
+    }
+
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.ED8E8FE3-96E9-058E-1DFB-86FF6076CCBE]
     // </editor-fold> 
-    public Grootte getGrootte () {
+    /**
+     * Geeft de grootte van het voertuig
+     * @return de grootte van het voertuig
+     */
+    public Grootte getGrootte() {
         return grootte;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.EF344CE1-BA05-214D-0B33-F016507FB6E2]
     // </editor-fold> 
-    public void setGrootte (Grootte val) {
+    /**
+     * Stelt de grootte van het voertuig in
+     * @param val de gewenste grootte
+     */
+    public void setGrootte(Grootte val) {
         this.grootte = val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.D74C6A0A-85DE-DA78-E7BC-E20D445D17F3]
     // </editor-fold> 
-    public Orientatie getOrientatie () {
+    /**
+     * Geeft de oriëntatie van het voertuig
+     * @return de oriëntatie van het voertuig
+     */
+    public Orientatie getOrientatie() {
         return orientatie;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.DDBD8211-092A-D0DB-F626-369F25EC155A]
     // </editor-fold> 
-    public void setOrientatie (Orientatie val) {
+    /**
+     * Stelt de oriëntatie van het voertuig in
+     * @param val de gewenste oriëntatie
+     */
+    public void setOrientatie(Orientatie val) {
         this.orientatie = val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.07868CD2-4BC0-9B22-2663-894106232A4F]
     // </editor-fold> 
-    public int getX () {
+    /**
+     * Geeft de X-coördinaat
+     * @return de X-coördinaat
+     */
+    public int getX() {
         return x;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.7A395978-B5D0-D622-8763-2D9F108B672A]
     // </editor-fold> 
-    public void setX (int val) {
+    /**
+     * Stelt de X-coördinaat in
+     * @param val de gewenste X-coördinaat
+     */
+    public void setX(int val) {
+        // TODO hier moeten nog checks uitgevoerd worden.
         this.x = val;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.5DFFBA52-8A94-6ADE-DBF1-B8208E7F03D0]
     // </editor-fold> 
-    public int getY () {
+    /**
+     * Geeft de Y-coördinaat
+     * @return de Y-coördinaat
+     */
+    public int getY() {
         return y;
     }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.2A69C894-15F3-9570-57FD-8442E6BF7BA9]
     // </editor-fold> 
-    public void setY (int val) {
+    /**
+     * Stelt de Y-coördinaat in
+     * @param val de Y-coördinaat
+     */
+    public void setY(int val) {
+        // TODO hier moeten nog checks uitgevoerd worden.
         this.y = val;
     }
 
@@ -109,11 +164,14 @@ public class Voertuig {
      *  <p style="margin-top: 0">
      *        Stel de positie in
      *      </p>
+     * @param X de X-coördinaat
+     * @param Y de Y-coördinaat
      */
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.B1AD730E-66D4-8101-094D-90D4ACA88853]
     // </editor-fold> 
     public void setPosition (int X, int Y) {
+        // TODO hier moeten nog checks uitgevoerd worden.
         this.x = X;
         this.y = Y;
     }
