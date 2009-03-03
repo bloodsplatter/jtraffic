53654e8d9dfb61947f3e4e15663b5cf4829bb7de
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-03-03 11:08:20 +0000

    Added high scores and level manager to experimental branch

---
+++ branches/experiments/jtraffic/src/jtraffic/HighScores.java
@@ -0,0 +1,98 @@
+
+package jtraffic;
+
+import RushHour.Level;
+import java.util.ArrayList;
+import java.io.*;
+
+/**
+ * Beheerd de high scores
+ * @author bloodsplatter
+ * @version 2009.03.03
+ */
+public class HighScores {
+    private static String appDir = System.getProperty("user.dir");
+    private static ArrayList<HighScoreRecord> highScoreTable;
+    private final static String fileName = "highscores.dat";
+    private static boolean initialized = false;
+
+    /**
+     * Initialiseerd de High Scores
+     * @throws java.io.IOException
+     * @throws ClassNotFoundException
+     */
+    public static void Initialize() throws IOException, ClassNotFoundException
+    {
+        if (!initialized)
+        {
+            File bestand = new File(appDir + "\\" + fileName);
+            if (!bestand.exists())
+            {
+                bestand.createNewFile();
+                highScoreTable = new ArrayList<HighScoreRecord>(1);
+                FileOutputStream fos = new FileOutputStream(bestand);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(highScoreTable);
+                oos.close();
+                fos.close();
+                initialized = true;
+            }
+            else
+            {
+                FileInputStream fis = new FileInputStream(bestand);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                highScoreTable = (ArrayList<HighScoreRecord>)ois.readObject();
+                ois.close();
+                fis.close();
+                initialized = true;
+            }
+        }
+    }
+
+    
+    /**
+     * Voegt een high score toe aan de tabel
+     * @param hsr de high score om toe te voegen
+     * @throws java.lang.Exception
+     */
+    public static void voegHighScoreToe(HighScoreRecord hsr) throws Exception
+    {
+        if(!initialized)
+            throw new Exception("Class must be initialised first");
+
+        highScoreTable.add(hsr);
+    }
+
+
+    /**
+     * Voegt een high score toe aan de tabel
+     * @param playerNaam naam van de speler die de score behaalde
+     * @param level de level waar de score behaald is
+     * @param score de score die behaald is
+     * @throws java.lang.Exception
+     */
+    public static void voegHighScoreToe(String playerNaam, Level level,int score) throws Exception
+    {
+        HighScoreRecord hsr = new HighScoreRecord(score, level.getNaam(), playerNaam);
+        voegHighScoreToe(hsr);
+    }
+
+
+    /**
+     * Slaagt de high scores op
+     * @throws java.io.IOException
+     * @throws java.lang.Exception een exception wordt gegooid als<br/>de klasse nog niet geïnitialiseerd is
+     */
+    public static void opslaan() throws IOException, Exception
+    {
+        if (!initialized)
+            throw new Exception("Class must be initialised first");
+
+        File bestand = new File(appDir + "\\" + fileName);
+        FileOutputStream fos = new FileOutputStream(bestand);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(highScoreTable);
+        oos.close();
+        fos.close();
+    }
+}
---
+++ branches/experiments/jtraffic/src/jtraffic/LevelManager.java
@@ -0,0 +1,85 @@
+package jtraffic;
+
+import RushHour.Level;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Beheert de levels
+ * @author bloodsplatter
+ * @version 2009.03.03
+ */
+public class LevelManager {
+    private static boolean initialized = false;
+    private static ArrayList<Level> levels;
+    private static String appDir = System.getProperty("user.dir");
+    private static final String fileName = "levels.dat";
+
+    /**
+     * Laad de level lijst of maakt een lege lijst aan
+     * @throws java.io.IOException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void Initialize() throws IOException, ClassNotFoundException
+    {
+        if (!initialized)
+        {
+            File bestand = new File(appDir + "\\" + fileName);
+            if (!bestand.exists())
+            {
+                bestand.createNewFile();
+                levels = new ArrayList<Level>(3);
+                FileOutputStream fos = new FileOutputStream(bestand);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(levels);
+                oos.close();
+                fos.close();
+                initialized = true;
+            }
+            else
+            {
+                FileInputStream fis = new FileInputStream(bestand);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                levels = (ArrayList<Level>)ois.readObject();
+                ois.close();
+                fis.close();
+                initialized = true;
+            }
+        }
+    }
+
+    /**
+     * Sla de levellijst op
+     * @throws java.lang.Exception gooit een exception als<br/>de klasse nog niet geïnitialiseerd is
+     */
+    public static void opslaan() throws Exception
+    {
+        if (!initialized)
+            throw new Exception("Class is not initialized");
+
+        File bestand = new File(appDir + "\\" + fileName);
+        FileOutputStream fos = new FileOutputStream(bestand);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(oos);
+        oos.close();
+        fos.close();
+    }
+
+    /**
+     * Voeg een level toe aan de lijst
+     * @param level de level die toegevoegd moet worden
+     * @throws java.lang.Exception gooit een exception als<br/>de klasse nog niet geïnitialiseerd is
+     */
+    public static void voegLevelToe(Level level) throws Exception
+    {
+        if (!initialized)
+           throw new Exception("Class is not initialized");
+
+        levels.add(level);
+    }
+}
