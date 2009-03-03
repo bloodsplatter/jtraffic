9bc830c40539bc243cdc8b8d89177b610b5aa7f4
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-03-03 09:15:24 +0000

    Added High Score system

---
+++ jtraffic/src/jtraffic/HighScoreRecord.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic;
+
+/**
+ *
+ * @author bloodsplatter
+ */
+public class HighScoreRecord {
+    private int stappen;
+    private String levelnaam;
+    private String spelernaam;
+
+    /**
+     * Constructor
+     * @param levelnaam de naam van de level waar de score is behaald
+     * @param spelernaam de naam van de speler die de score behaalde
+     */
+    public HighScoreRecord(String levelnaam, String spelernaam) {
+        this.levelnaam = levelnaam;
+        this.spelernaam = spelernaam;
+        this.stappen = 0;
+    }
+
+    /**
+     * Constructor
+     * @param steps het aantal stappen dat nodig was om de level te doorlopen
+     * @param levelnaam de naam van de level waar de score is behaald
+     * @param spelernaam de naam van de speler die de score behaalde
+     */
+    public HighScoreRecord(int steps, String levelnaam, String spelernaam) {
+        this.stappen = steps;
+        this.levelnaam = levelnaam;
+        this.spelernaam = spelernaam;
+    }
+
+    /**
+     * Geeft de naam van de level waar de score is behaald
+     * @return de naam van de level waar de score is behaald
+     */
+    public String getLevelnaam() {
+        return levelnaam;
+    }
+
+    /**
+     * stel de naam in van de level waar de score is behaald
+     * @param levelnaam de naam van de level
+     */
+    public void setLevelnaam(String levelnaam) {
+        this.levelnaam = levelnaam;
+    }
+
+    /**
+     * Stel de naam van de speler in
+     * @return de naam van de speler
+     */
+    public String getSpelernaam() {
+        return spelernaam;
+    }
+
+    /**
+     * Stel de naam in van de speler
+     * @param spelernaam de naam van de speler
+     */
+    public void setSpelernaam(String spelernaam) {
+        this.spelernaam = spelernaam;
+    }
+
+    /**
+     * Vraag het aantal stappen op dat nodig was om de level op te lossen
+     * @return het aantal stappen
+     */
+    public int getSteps() {
+        return stappen;
+    }
+
+    /**
+     * Stel het aantal stappen in dat nodig was om de level op te lossen
+     * @param steps het aantal benodigde stappen
+     */
+    public void setSteps(int steps) {
+        this.stappen = steps;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("|%1$4d|%2$c|%3$c|",String.valueOf(stappen),levelnaam,spelernaam);
+    }
+
+    
+}
---
+++ jtraffic/src/jtraffic/HighScores.java
@@ -0,0 +1,78 @@
+
+package jtraffic;
+
+import RushHour.Level;
+import java.util.ArrayList;
+import java.io.*;
+
+/**
+ * Beheerd de high scores
+ * @author bloodsplatter
+ */
+public class HighScores {
+    private static String appDir = System.getProperty("user.dir");
+    private static ArrayList<HighScoreRecord> highScoreTable;
+    private final static String fileName = "highscores.dat";
+    private static boolean initialised = false;
+
+    /**
+     * Initialiseerd de High Scores
+     * @throws java.io.IOException
+     * @throws ClassNotFoundException
+     */
+    public static void Initialize() throws IOException, ClassNotFoundException
+    {
+        if (!initialised)
+        {
+            File bestand = new File(appDir + "\\" + fileName);
+            if (!bestand.exists())
+            {
+                bestand.createNewFile();
+                highScoreTable = new ArrayList<HighScoreRecord>(1);
+                FileOutputStream fos = new FileOutputStream(bestand);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(highScoreTable);
+                oos.close();
+                fos.close();
+                initialised = true;
+            }
+            else
+            {
+                FileInputStream fos = new FileInputStream(bestand);
+                ObjectInputStream ois = new ObjectInputStream(fos);
+                highScoreTable = (ArrayList<HighScoreRecord>)ois.readObject();
+                ois.close();
+                fos.close();
+                initialised = true;
+            }
+        }
+    }
+
+    
+    /**
+     * Voegt een high score toe aan de tabel
+     * @param hsr de high score om toe te voegen
+     * @throws java.lang.Exception
+     */
+    public static void AddHighScore(HighScoreRecord hsr) throws Exception
+    {
+        if(!initialised)
+            throw new Exception("Class must be initialised first");
+
+        highScoreTable.add(hsr);
+    }
+
+
+    /**
+     * Voegt een high score toe aan de tabel
+     * @param playerNaam
+     * @param level
+     * @param score
+     * @throws java.lang.Exception
+     */
+    public static void AddHighScore(String playerNaam, Level level,int score) throws Exception
+    {
+        HighScoreRecord hsr = new HighScoreRecord(score, level.getNaam(), playerNaam);
+        AddHighScore(hsr);
+    }
+}
