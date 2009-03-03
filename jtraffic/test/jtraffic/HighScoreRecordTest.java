3d335ab3dae46184ca64f4689d329bc1a02c36f7
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-03-03 09:18:56 +0000

    HighScoreRecordTest for it's toString method

---
+++ jtraffic/test/jtraffic/HighScoreRecordTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bloodsplatter
+ */
+public class HighScoreRecordTest {
+
+    public HighScoreRecordTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of toString method, of class HighScoreRecord.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        HighScoreRecord instance = new HighScoreRecord(5, "Testlevel", "Jonas");
+        String expResult = "|   5|Testlevel|Jonas|";
+        String result = instance.toString();
+        try {
+            assertEquals(expResult, result);
+        } catch (Exception e) {
+            fail("Exception: " + e.toString()+ "\nReturn value:"+result);
+        }
+        
+    }
+
+}
