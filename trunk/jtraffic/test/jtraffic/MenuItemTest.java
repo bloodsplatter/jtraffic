29abbacfe0bf78b39a4c2b2e2d19de7e7efe1bca
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-04-19 09:25:21 +0000

    * Added test for MenuItem

---
+++ trunk/jtraffic/test/jtraffic/MenuItemTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtraffic;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bloodsplatter
+ */
+public class MenuItemTest {
+
+    public MenuItemTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of getNaam method, of class MenuItem.
+     */
+    @Test
+    public void testGetNaam() {
+        System.out.println("getNaam");
+        MenuItem instance = new MenuItem() {
+
+            @Override
+            public void doAction() {
+                System.out.println("An action ocurred.");
+            }
+        };
+        instance.setNaam("Click action");
+        String expResult = "Click action";
+        String result = instance.getNaam();
+        try {
+            assertEquals(expResult, result);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        
+    }
+
+    /**
+     * Test of toString method, of class MenuItem.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        MenuItem instance = new MenuItem() {
+
+            @Override
+            public void doAction() {
+                return;
+            }
+        };
+        instance.setNaam("testnaam");
+        String expResult = "testnaam";
+        String result = instance.toString();
+        try {
+            assertEquals(expResult, result);
+        } catch (Exception e) {
+            fail("Test failed");
+        }
+    }
+
+}
