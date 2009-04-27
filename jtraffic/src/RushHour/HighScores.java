503b7a99e18efa5ffd357bf58a9a981c0181432a
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-04-27 10:27:48 +0000

    Applied the Singleton design server

--- jtraffic/src/RushHour/HighScores.java
+++ jtraffic/src/RushHour/HighScores.java
@@ -1,161 +1,148 @@
 package RushHour;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.io.File;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  * Beheerd de high scores
  * @author bloodsplatter
- * @version 2009.04.21
+ * @version 2009.04.27
  */
 public class HighScores {
-
-    private static String appDir = System.getProperty("user.dir");
-    private static ArrayList<HighScoreRecord> highScoreTable;
-    private final static String fileName = "highscores.dat";
-    private static boolean initialized = false;
+    private static HighScores _instance = null;
+    private ArrayList<HighScoreRecord> highScores;
 
     /**
-     * Initialiseerd de High Scores
-     * @throws java.io.IOException
-     * @throws ClassNotFoundException
+     * Constructor
      */
-    public static void Initialize() throws IOException, ClassNotFoundException {
-        if (!initialized) {
-            File bestand = new File(appDir + "\\" + fileName);
-            if (!bestand.exists()) {
-                bestand.createNewFile();
-                highScoreTable = new ArrayList<HighScoreRecord>(1);
-                FileOutputStream fos = new FileOutputStream(bestand);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                oos.writeObject(highScoreTable);
-                oos.close();
-                fos.close();
-                initialized = true;
-            } else {
-                FileInputStream fis = new FileInputStream(bestand);
+    private HighScores () throws Exception
+    {
+        this.highScores = new ArrayList<HighScoreRecord>();
+        File dat = new File("highscores.dat");
+        if (!dat.exists())
+        {
+            try {
+                dat.createNewFile();
+            } catch (IOException ioex)
+            {
+                // TODO could not create empty file
+            }
+        } else
+        {
+            try
+            {
+                FileInputStream fis = new FileInputStream(dat);
                 ObjectInputStream ois = new ObjectInputStream(fis);
-                highScoreTable = (ArrayList<HighScoreRecord>) ois.readObject();
-                ois.close();
-                fis.close();
-                initialized = true;
+                highScores = (ArrayList<HighScoreRecord>)ois.readObject();
+            } catch (FileNotFoundException fnfe)
+            {
+                // should not happen
+            } catch (IOException ioe)
+            {
+                // also should not happen
+            } catch (ClassNotFoundException cnfe)
+            {
+                throw new Exception("highscores.dat has been tampered with!");
             }
         }
     }
 
     /**
-     * Voegt een high score toe aan de tabel
-     * @param hsr de high score om toe te voegen
+     * Maakt een instance aan
      */
-    public static void voegHighScoreToe(HighScoreRecord hsr){
-        if (!initialized) {
-            return;
-        }
+    private synchronized static void createInstance() throws Exception
+    {
+        if (_instance == null) _instance = new HighScores();
+    }
 
-        if (highScoreTable.size() <= 10) {
-            highScoreTable.add(hsr);
-        } else {
-            
-            Iterator<HighScoreRecord> hsrIterator = highScoreTable.iterator();
-            while (hsrIterator.hasNext()) {
-                HighScoreRecord current = hsrIterator.next();
-                if (current.getSteps() <= hsr.getSteps()) {
-                    highScoreTable.remove(highScoreTable.size() - 1);
-                    highScoreTable.add(hsr);
+    /**
+     * Geeft een instantie van de klasse
+     * @return de instantie van de klasse
+     */
+    public static HighScores getInstance() throws Exception
+    {
+        if (_instance == null)
+            createInstance();
+        
+        return _instance;
+    }
+
+    /**
+     * Verwijder de highscores uit de lijst
+     */
+    public void verwijderHighScores()
+    {
+        highScores.clear();
+    }
+
+    /**
+     * Voegt een HighScoreRecord toe
+     * @param hsr het HighScoreRecord om toe te voegen
+     * @see RushHour.HighScoreRecord
+     */
+    public void voegHighScoreToe(HighScoreRecord hsr)
+    {
+        if (highScores.size() >= 10)
+        {
+            Collections.sort(highScores);
+            Iterator<HighScoreRecord> itr = highScores.iterator();
+            while (itr.hasNext())
+            {
+                HighScoreRecord tmp = itr.next();
+                if (tmp.getSteps() < hsr.getSteps())
+                {
+                    int index = highScores.indexOf(tmp);
+                    highScores.remove(tmp);
+                    highScores.add(index, hsr);
+                    break;
                 }
             }
-        }
-    }
-
-    /**
-     * Verwijderd een HighScoreRecord uit de tabel
-     * @param hsr de record die verwijderd moet worden
-     */
-    public static void verwijderHighScore(HighScoreRecord hsr){
-        if (!initialized) {
-            return;
-        }
-
-        if (highScoreTable.contains(hsr)) {
-            highScoreTable.remove(hsr);
-        }
-    }
-
-    /**
-     * Voegt een high score toe aan de tabel
-     * @param playerNaam naam van de speler die de score behaalde
-     * @param level de level waar de score behaald is
-     * @param score de score die behaald is
-     */
-    public static void voegHighScoreToe(String playerNaam, Level level, int score) {
-        HighScoreRecord hsr = new HighScoreRecord(score, level.getNaam(), playerNaam);
-        voegHighScoreToe(hsr);
-    }
-
-    /**
-     * Slaagt de high scores op
-     * @throws java.io.IOException
-     * @throws java.lang.Exception een foutmelding wordt opgegooid als<br/>de klasse nog niet geïnitialiseerd is
-     */
-    public static void opslaan() throws IOException, Exception {
-        if (!initialized) {
-            throw new Exception("Class must be initialised first");
-        }
-
-        File bestand = new File(appDir + "\\" + fileName);
-        FileOutputStream fos = new FileOutputStream(bestand);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(highScoreTable);
-        oos.close();
-        fos.close();
-    }
-
-    /**
-     * Geeft aan of de klasse al geïnitialiseerd is
-     * @return true als de klasse geïnitialiseerd is, anders false
-     */
-    public static boolean isInitialized() {
-        return initialized;
-    }
-
-    /**
-     * Vraagt een record op een plaats op
-     * @param plaats de plaats van het record
-     * @return null als de klasse nog niet geïnitialiseerd is<br/>of de locatie niet geldig is.<br/>anderes het record
-     */
-    public static HighScoreRecord highScoreOpNummer(int plaats) {
-        if (initialized) {
-            if (plaats >= 0 && plaats < highScoreTable.size())
-                return highScoreTable.get(plaats);
-        }
-        return null;
-    }
-
-    /**
-     * Verwijdert alle highscores
-     */
-    public static void verwijderAlleHighScores()
-    {
-        if(initialized)
+        } else
         {
-             highScoreTable = new ArrayList<HighScoreRecord>(10);
+            highScores.add(hsr);
         }
     }
 
     /**
-     * Geeft een array met alle HighScoreRecords
+     * Voegt een HighScoreRecord toe op basis van level en spelernaam
+     * @param level de level waar de highscore is behaalt
+     * @param spelernaam de naam van de speler die de highscore behaalde
+     */
+    public void voegHighScoreToe(Level level, String spelernaam)
+    {
+        HighScoreRecord hsr = new HighScoreRecord(level.getAantalStappen(), level.getNaam(), spelernaam);
+        this.voegHighScoreToe(hsr);
+    }
+
+    /**
+     * Maak een array met alle HighScoreRecords
      * @return een array met alle HighScoreRecords
      */
-    public static HighScoreRecord[] toArray()
+    public HighScoreRecord[] toArray()
     {
-        HighScoreRecord[] hsrs = new HighScoreRecord[highScoreTable.size()];
-        highScoreTable.toArray(hsrs);
+        HighScoreRecord[] hsrs = new HighScoreRecord[highScores.size()];
+        highScores.toArray(hsrs);
         return hsrs;
+    }
+
+    /**
+     * Sla de highscores op
+     */
+    public void opslaan() throws FileNotFoundException, IOException
+    {
+        File dat = new File("highscores.dat");
+        FileOutputStream fos = new FileOutputStream(dat);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(highScores);
+        oos.close();
+        fos.close();
     }
 }
