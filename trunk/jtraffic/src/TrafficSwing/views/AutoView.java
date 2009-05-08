ad9c52b4fbca8cbb54b226e9797efae5c573b5f5
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-05-08 13:02:45 +0000

    Added more resources, modified the resourcemanager and both views

--- trunk/jtraffic/src/TrafficSwing/views/AutoView.java
+++ trunk/jtraffic/src/TrafficSwing/views/AutoView.java
@@ -5,14 +5,21 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import RushHour.*;
+import TrafficSwing.util.ResourceManager;
 
 /**
  * De view klasse voor Auto
  * @author bloodsplatter
  * @version 2009.05.08
  */
-public class AutoView extends JLabel {
+public class AutoView extends JPanel {
     protected Auto auto;
+    protected ImageIcon img;
+    //left or up
+    protected JButton dirButton;
+    // right or down
+    protected JButton odirButton;
+    
 
     /**
      * Constructor
@@ -21,7 +28,49 @@ public class AutoView extends JLabel {
      */
     public AutoView(Orientatie orientatie,Kleur kleur)
     {
+        super(new BorderLayout());
         auto = new Auto(orientatie);
         auto.setKleur(kleur);
+        img = ResourceManager.getInstance().getAfbeeldingVoorVoertuig(auto);
+        super.add(new JLabel(img), BorderLayout.CENTER);
+        if (auto.getOrientatie() == Orientatie.Horizontaal)
+        {
+            // knop naar links
+            dirButton = new JButton(ResourceManager.getInstance().getPijl(ResourceManager.PIJL_LINKS));
+            dirButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    auto.NaarLinks();
+                }
+            });
+
+            // knop naar rechts
+            odirButton = new JButton(ResourceManager.getInstance().getPijl(ResourceManager.PIJL_RECHTS));
+            odirButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    auto.NaarRechts();
+                }
+            });
+        } else
+        {
+            // knop naar boven
+            dirButton = new JButton(ResourceManager.getInstance().getPijl(ResourceManager.PIJL_BOVEN));
+            dirButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    auto.NaarBoven();
+                }
+            });
+
+            // knop naar rechts
+            odirButton = new JButton(ResourceManager.getInstance().getPijl(ResourceManager.PIJL_BENEDEN));
+            odirButton.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    auto.NaarBeneden();
+                }
+            });
+        }
     }
 }
