4565f2a301028816a64f197b10f9542b1fccb4fd
Cvermeiren <Cvermeiren@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-05-12 10:51:41 +0000

    lvl view added

---
+++ jtraffic/src/TrafficSwing/views/LevelView.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TrafficSwing.views;
+
+/**
+ *
+ * @author Chris
+ * @version 2009.05.12
+ */
+public class LevelView extends View {
+
+    @Override
+    public void sluitRequest() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean kanSluiten() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void sluit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+
+
+
+}
