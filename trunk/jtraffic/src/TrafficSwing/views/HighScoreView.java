65d6a7244d4fc0f7c5fce98a62c4e77b8e9e6c28
buttock.scout <buttock.scout@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-05-15 07:17:43 +0000

    wharrgarbl

--- trunk/jtraffic/src/TrafficSwing/views/HighScoreView.java
+++ trunk/jtraffic/src/TrafficSwing/views/HighScoreView.java
@@ -9,9 +9,9 @@ import java.awt.event.*;
 /**
  * De viewklasse voor de high scores
  * @author bloodsplatter
- * @version 2009.05.12
+ * @version 2009.05.14
  */
-public class HighScoreView extends SideView {
+public class HighScoreView extends JFrame {
     protected HighScoresTableModel hstm;
     protected JTable tabel;
     protected JScrollPane scrollPane;
@@ -23,19 +23,52 @@ public class HighScoreView extends SideView {
      */
     public HighScoreView()
     {
-        super(new BorderLayout());
-        frame.setTitle("High Scores");
+        super("High Scores");
+        super.setLayout(new BorderLayout());
         initTable();
 
         bottomPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
         
         okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
         bottomPane.add(okButton);
 
         super.add(bottomPane,BorderLayout.SOUTH);
-        frame.add(this);
-        frame.pack();
-        frame.setVisible(true);
+        debug();
+        super.add(scrollPane,BorderLayout.CENTER);
+        super.pack();
+        super.setVisible(true);
+    }
+
+    /**
+     * Adds a debug menu to the menubar
+     */
+    private void debug()
+    {
+        JMenuBar menuBar;
+        if (super.getJMenuBar() == null)
+        {
+            menuBar = new JMenuBar();
+            super.setJMenuBar(menuBar);
+        }
+        else
+            menuBar = super.getJMenuBar();
+
+        JMenu debugMenu = new JMenu("Debug");
+        menuBar.add(debugMenu);
+        JMenuItem addHighScore = new JMenuItem("Voeg high score toe");
+        debugMenu.add(addHighScore);
+        addHighScore.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                voegHighScoreToe();
+            }
+        });
     }
 
     protected void initTable()
@@ -47,8 +80,22 @@ public class HighScoreView extends SideView {
         super.add(scrollPane,BorderLayout.CENTER);
     }
 
-    @Override
-    public boolean kanSluiten() {
-        return super.kanSluiten();
+    public void voegHighScoreToe()
+    {
+        try {
+            //Level lvl = Application.getInstance().getHuidigeLevel();
+            String gebruikersnaam = null;
+            do {
+                gebruikersnaam = JOptionPane.showInputDialog(Application.getInstance(), "Geef uw naam in", "Naam speler", JOptionPane.INFORMATION_MESSAGE);
+            } while (gebruikersnaam == null);
+
+            HighScoreRecord hsr = new HighScoreRecord("Testlevel", gebruikersnaam);
+            hsr.setSteps(5);
+            HighScores.getInstance().voegHighScoreToe(hsr);
+            scrollPane.updateUI();
+        } catch (UnsupportedOperationException uoe)
+        {
+            JOptionPane.showMessageDialog(Application.getInstance(), uoe.getMessage(), "Fout", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
