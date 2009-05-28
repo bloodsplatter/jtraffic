47a4005b9106dba122b2f7b6e07fabc7ab2bf120
cvermeiren@gmail.com <cvermeiren@gmail.com@b73b0150-f9d8-11dd-a7be-4b3ef3b0700c>
2009-05-28 11:13:59 +0000

    RC ai bug maar niet interesant meer voor te fixen

--- branches/experiments/jtraffic/src/jtraffic/Main.java
+++ branches/experiments/jtraffic/src/jtraffic/Main.java
@@ -35,41 +35,60 @@ public class Main {
         vrt3.setKleur('A');
         lvl.voegVoertuigToe(vrt3);
 
-        Vrachtwagen vrt1 = new Vrachtwagen(Orientatie.Horizontaal);
+        Auto vrt1 = new Auto(Orientatie.Horizontaal);
         vrt1.setX(0);
         vrt1.setY(1);
         vrt1.setKleur('B');
         lvl.voegVoertuigToe(vrt1);
 
         Auto vrt4 = new Auto(Orientatie.Verticaal);
-        vrt4.setX(0);
-        vrt4.setY(4);
+        vrt4.setX(2);
+        vrt4.setY(0);
         vrt4.setKleur('C');
         lvl.voegVoertuigToe(vrt4);
 
-        Auto vrt5 = new Auto(Orientatie.Horizontaal);
-        vrt5.setX(4);
-        vrt5.setY(4);
+        Vrachtwagen vrt5 = new Vrachtwagen(Orientatie.Verticaal);
+        vrt5.setX(0);
+        vrt5.setY(2);
         vrt5.setKleur('D');
         lvl.voegVoertuigToe(vrt5);
 
         Vrachtwagen vrt6 = new Vrachtwagen(Orientatie.Horizontaal);
-        vrt6.setX(2);
-        vrt6.setY(5);
+        vrt6.setX(1);
+        vrt6.setY(3);
         vrt6.setKleur('E');
         lvl.voegVoertuigToe(vrt6);
 
-        Vrachtwagen vrt7 = new Vrachtwagen(Orientatie.Verticaal);
-        vrt7.setX(3);
-        vrt7.setY(1);
+        Auto vrt7 = new Auto(Orientatie.Horizontaal);
+        vrt7.setX(4);
+        vrt7.setY(0);
         vrt7.setKleur('F');
         lvl.voegVoertuigToe(vrt7);
 
         Vrachtwagen vrt8 = new Vrachtwagen(Orientatie.Verticaal);
         vrt8.setX(5);
-        vrt8.setY(0);
+        vrt8.setY(1);
         vrt8.setKleur('G');
         lvl.voegVoertuigToe(vrt8);
+
+        Auto vrt9 = new Auto(Orientatie.Horizontaal);
+        vrt9.setX(4);
+        vrt9.setY(4);
+        vrt9.setKleur('H');
+        lvl.voegVoertuigToe(vrt9);
+
+        Auto vrt10 = new Auto(Orientatie.Horizontaal);
+        vrt10.setX(4);
+        vrt10.setY(5);
+        vrt10.setKleur('I');
+        lvl.voegVoertuigToe(vrt10);
+
+        Auto vrt11 = new Auto(Orientatie.Verticaal);
+        vrt11.setX(3);
+        vrt11.setY(4);
+        vrt11.setKleur('J');
+        lvl.voegVoertuigToe(vrt11);
+
 
 
         lvl.maakSnapshot();
